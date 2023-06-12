8274712f403a7a5cde2a09d23df2d85cdbe9ce33
云微 <1067852565@qq.com>
2023-06-12 13:45:13 +0000

    add test for user level api

--- __tests__/statsFetcher.test.js
+++ __tests__/statsFetcher.test.js
@@ -2,7 +2,8 @@ import { fetchStats } from '../lib/statsFetcher';
 
 describe('fetchStats', () => {
   it('returns an object with the expected properties', async () => {
-    const stats = await fetchStats('yunwei');
+    const stats = await fetchStats('yunwei37');
+    console.log(JSON.stringify(stats));
     expect(stats).toHaveProperty('totalCommits');
     expect(stats).toHaveProperty('totalPRs');
     expect(stats).toHaveProperty('totalIssues');

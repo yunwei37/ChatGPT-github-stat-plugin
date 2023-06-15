08c3cffb52524f8f9cd8b74af9c9ae25d1ea45bd
云微 <1067852565@qq.com>
2023-06-15 12:15:55 +0000

    change the return code

--- pages/api/github/[owner]/[repo].ts
+++ pages/api/github/[owner]/[repo].ts
@@ -8,6 +8,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     
     res.status(200).json(data);
   } catch (error: any) {
-    res.status(500).json({ error: 'Unexpected error.' + error.message });
+    res.status(404).json({ error: 'Unexpected error.' + error.message });
   }
 }
